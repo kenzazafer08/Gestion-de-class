0e84de908bdd10a075c98368302a734592dfedb7
unknown <zaaferkenza08@gmail.com>
2022-09-16 11:49:45 +0100

    done

--- function.h
+++ function.h
@@ -6,12 +6,15 @@ struct Date {
     };
     struct Etudiant {
         char name [30] ;
+        int prenom[30];
         int age;
-        char niveaux [30] ;
         struct Date date; 
-        int note[4];
-        int moyenne;
     };
+int age(struct Date date){
+     int CD=2022;
+     int age=date.annee-CD;
+     return age;
+}
 struct Etudiant* saisie(struct Etudiant *tab,int N){
     int i,j,somme[N];
     for (i = 0; i < N; i++)
@@ -20,42 +23,23 @@ struct Etudiant* saisie(struct Etudiant *tab,int N){
         printf("\n\t---Etudiant %d---\n",i+1);
         printf ("Nom : ");
         scanf ("%s", tab[i].name);
-        printf ("Age : ");
-        scanf ("%d", &tab[i].age);
-        printf ("Niveaux : ");
-        scanf ("%s", tab[i].niveaux);
+        printf ("Prenom : ");
+        scanf ("%s", tab[i].prenom);
         printf ("Date de naissance : \n");
         printf ("Jour : ");
         scanf ("%d", &tab[i].date.jour);
         printf ("Mois : ");
         scanf ("%d", &tab[i].date.mois);
         printf ("Annee : ");
-        scanf ("%d", &tab[i].date.annee);
-        printf("Notes : \n");
-        for (j =0; j < 4; j++)
-        {
-          printf ("Note %d : ", j+1, i+1);
-          scanf ("%d", &tab[i].note[j]);
-          somme[i]=somme[i]+(tab[i].note[j]);
+        scanf ("%d", &tab[i].date.annee);    
+        tab[i].age = age(tab[i].date);
         }
-        tab[i].moyenne=somme[i]/4;
-    }
     return tab;
 }
 void affichage(struct Etudiant *tab){
     int j;
        printf("\nNom : %s",(tab)->name);
-       printf("\nAge : %d",tab->age);
-       printf("\nNiveaux : %s",(tab)->niveaux);
+       printf("\nPrenom : %s",(tab)->prenom);
        printf("\nDate de naissance : %d/%d/%d",tab->date.jour,tab->date.mois,tab->date.annee);
-       printf("\nNotes : ");
-       for(j=0;j<4;j++){
-          printf("\nNote %d : %d",j+1,(tab)->note[j]);
-       }
-       if(tab->moyenne >= 12){
-          printf("\nMoyenne : %d - Admis",tab->moyenne);
-       }else if (tab->moyenne > 8 && tab->moyenne < 12){
-          printf("\nMoyenne : %d - Redoublons",tab->moyenne);
-       }else  printf("\nMoyenne : %d - Exclus",tab->moyenne);
-    
+       printf("\nAge : %d",tab->date.jour,tab->date.mois,tab->date.annee);
 }
--- test.c
+++ test.c
@@ -1,21 +1,40 @@
 #include <stdio.h>
-void main(){
+    struct Date {
+        int jour;
+        int mois;
+        int annee;
+    };
     struct Etudiant
     {
        char name[10];
        int age;
-       char niveaux[20];
+       char prenom[20];
+       struct Date d;
     };
+    int age(struct Date date){
+     int CD=2022;
+     int age=date.annee-CD;
+     return age;
+    }
+    
+void main(){
     struct Etudiant E;
     printf("Entrer vos informations : ");
-    printf("\nVotre nom : ");
-    scanf("%s",&E.name);
-    printf("Votre age : ");
-    scanf("%d",&E.age);
-    printf("Votre niveau : ");
-    scanf("%s",&E.niveaux);
+    printf("\nNom : ");
+    scanf("%s",E.name);
+    printf("Prenom : ");
+    scanf("%d",E.prenom);
+    printf("Date de naissance : \n");
+    printf("Jour : ");
+    printf("%d",&E.d.jour);
+    printf("Mois : ");
+    printf("%d",&E.d.mois);
+    printf("Annee : ");
+    printf("%d",&E.d.annee);
+    E.age=age(E.d);
     printf("\n\n ---Bonjour---");
     printf("\nNom : %s",E.name);
-    printf("\nAge : %d",E.age);
-    printf("\nNiveau : %s",E.niveaux);
+    printf("\nPrenom : %d",E.prenom);
+    printf("\nDate de naissance : %d/%d/%d",E.d.jour,E.d.mois,E.d.annee);
+    printf("\nPrenom : %d",E.prenom);
 }
